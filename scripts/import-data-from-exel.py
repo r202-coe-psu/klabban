90f9e4cf075871cf9aafc23324463fc32bf7f13b
Thanathip Limna <boatkrap@gmail.com>
2025-11-26 09:00:49 +0700

    begin add import script

---
+++ scripts/import-data-from-exel.py
@@ -0,0 +1,35 @@
+import mongoengine as me
+import pandas
+import sys
+from klabban.models import User, Refugee
+
+
+def import_excel(base_path="."):
+    user = User.objects(username="admin").first()
+
+    if not user:
+        print("admin user not found, please run init-admin.py first")
+        return
+    
+    pd = pandas.read_excel(f"{base_path}/data/refugees.xlsx")
+
+    for index, row in pd.iterrows():
+        refugee = Refugee(
+            name=row.get("name"),
+            nick_name=row.get("nick_name", "")
+        )
+        refugee.save()
+
+    
+
+
+if __name__ == "__main__":
+    DB_NAME = "klabbandb"
+    if len(sys.argv) > 1:
+        me.connect(db=DB_NAME, host=sys.argv[1])
+    else:
+        me.connect(db=DB_NAME)
+    print(f"connect to {DB_NAME}")
+
+    import_excel()
+    print("======= import excel success ========")
