296669038efe248cbb8d622011110dcbcdf198e2
alkaline1024 <earthza991@hotmail.com>
2025-11-27 00:41:23 +0700

    fix: 🐛 null country api list

--- klabban/web/views/api.py
+++ klabban/web/views/api.py
@@ -7,6 +7,5 @@ module = Blueprint("api", __name__)
 
 @module.route("/master/countries", methods=["GET", "POST"])
 def master_countries():
-    countries = list(set(models.Refugee.objects.distinct("country")))
-    countries.sort()
+    countries = list(models.Refugee.objects(country__ne=None).distinct("country"))
     return {"countries": countries}
