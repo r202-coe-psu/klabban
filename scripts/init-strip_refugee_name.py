e9cf0dfaad743ba368f32bad1ad78e631a18b555
Vrtzean <zeanxlaws@gmail.com>
2025-11-28 13:24:44 +0700

    feat: 🎸 count script

--- scripts/init-strip_refugee_name.py
+++ scripts/init-strip_refugee_name.py
@@ -6,9 +6,15 @@ from klabban.models import Refugee
 
 def strip_refugee_name():
     refugees = Refugee.objects()
+    count += 0
     for refugee in refugees:
+        old_name = refugee.name
         refugee.name = refugee.name.strip()
+        if old_name != refugee.name:
+            print(f"Updating refugee name from '{old_name}' to '{refugee.name}'")
+            count += 1
         refugee.save()
+    print(f"Total refugee names updated: {count}")
 
 if __name__ == "__main__":
     DB_NAME = "klabbandb"
