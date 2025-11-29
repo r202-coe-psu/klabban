bfa61fefe6b1afe1f1b5f75bf1c4aec87011ce0a
SaktanuthPeak <thanakrit.5122548@gmail.com>
2025-11-29 14:33:14 +0700

    add import missing person log model

--- klabban/models/missing_persons.py
+++ klabban/models/missing_persons.py
@@ -55,3 +55,19 @@ class MissingPerson(me.Document):
     )
     created_date = me.DateTimeField(default=datetime.datetime.now)
     updated_date = me.DateTimeField(default=datetime.datetime.now)
+
+
+class ImportMissingPerson(me.document):
+    meta = {
+        "collection": "import_missing_persons",
+        "indexes": ["created_date"],
+    }
+
+    file = me.FileField()
+    file_name = me.StringField(required=True)
+    record_count = me.IntField(default=0)
+    source = me.StringField(max_length=255)
+    error_messages = me.ListField(me.StringField())
+    uploaded_date = me.DateTimeField(default=datetime.datetime.now)
+    created_date = me.DateTimeField(default=datetime.datetime.now)
+    created_by = me.ReferenceField("User", required=True)
